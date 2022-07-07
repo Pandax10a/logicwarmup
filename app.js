3eadc537f6a5cadf84002b027b9ebc5de6d114e7
panda1 <blueblue0011@gmail.com>
2022-07-07 15:26:37 -0700

    step 10

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ let is_sold_out = tickets_sold >= venue_capacity;
 
 if (is_sold_out == true) {
     console.log("all sold out");
-} else if (tickets_sold/venue_capacity >= 0.9 ); {
-    console.log("almost sold out");
-} else if (tickets_sold/venue_capacity >= 0.5); {
-    console.log("tickets selling fast");
+} else if (tickets_sold > venue_capacity) {
+    console.log("system error");
 }
- else {
+ else if (tickets_sold/venue_capacity >= 0.9 ) {
+    console.log("almost sold out");
+} else if (tickets_sold/venue_capacity >= 0.5) {
+    console.log("tickets selling fast");
+} else {
     console.log("tickets on sale now");
 }
