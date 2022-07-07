4d4e2b83c4e2036541c21d037f0e06817e771fcb
panda1 <blueblue0011@gmail.com>
2022-07-07 16:55:23 -0700

    finished 9

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let tickets_sold = 200;
+let tickets_sold = 150;
 let venue_capacity = 200;
 let performer = "oscar";
-let is_sold_out = tickets_sold > venue_capacity;
+let is_sold_out = tickets_sold >= venue_capacity;
 let is_member = true;
 let user_points = 9;
 let user_name = "alex";
@@ -12,3 +12,10 @@ if (is_sold_out === true) {
     console.log("Tickets still available");
 }
 
+if (( tickets_sold/venue_capacity >= 0.9) && (is_sold_out === false)) {
+    console.log("almost sold out")
+} else if ((tickets_sold/venue_capacity >= 0.5) && (is_sold_out === false)) {
+    console.log("tickets selling fast");
+} else if (is_sold_out === false){
+    console.log("tickets on sale now");
+}
