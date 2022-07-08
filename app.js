dc6ea9ac5e684689048bea401247d11c3b9b5c12
panda1 <blueblue0011@gmail.com>
2022-07-07 17:31:19 -0700

    done

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ let user_points = 9;
 let user_name = "alex";
 let is_repeat_show = false;
 
+let special_performer_1 = "someone";
+let special_performer_2 = "Alex Bymoen";
+let special_performer_3 = "somewhat";
+let special_performer_4 = "1972 Miami dolphins";
+let special_performer_5 = "somehow";
+
 if (is_sold_out === true) {
     console.log("all sold out");
 } else {
@@ -37,4 +43,18 @@ if (((tickets_sold/venue_capacity >= 0.9) || (venue_capacity >= 60000)) && ((tic
     console.log("special case 2");
 } else {
     console.log("normal case");
+}
+
+if ((special_performer_1 === "Alex Bymoen") || (special_performer_1 === "Shrek") || (special_performer_1 ==="1972 Miami dolphins")) {
+    console.log("Lucky You!");
+} else if ((special_performer_2 === "Alex Bymoen") || (special_performer_2 === "Shrek") || (special_performer_2 ==="1972 Miami dolphins")) {
+    console.log("Lucky You!");
+} else if ((special_performer_3 === "Alex Bymoen") || (special_performer_3 === "Shrek") || (special_performer_3 ==="1972 Miami dolphins")) {
+    console.log("Lucky You!");
+} else if ((special_performer_4 === "Alex Bymoen") || (special_performer_4 === "Shrek") || (special_performer_4 ==="1972 Miami dolphins")) {
+    console.log("Lucky You!");
+} else if ((special_performer_5 === "Alex Bymoen") || (special_performer_5 === "Shrek") || (special_performer_5 ==="1972 Miami dolphins")) {
+    console.log("Lucky You!");
+} else {
+    console.log("too bad")
 }
